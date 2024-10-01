10d862055f5fb533a33211dcaa523fa08f725f4f
Sedra <SedraAD>
2024-10-01 16:44:05 +0200

    Title: Docker_Burger What: Created Api for my burger Docker_burger Why: To start designing it

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ app.get("/GitBurger", (req, res) => {
     res.send("GitBurger")
 })
 
-
 app.get("/Menu", (req, res) => {
     res.render("Menu")
 })
 
 app.get("/VS_Burger", (req, res) => {
     res.render("Vs_burger")
+})
+
+app.get("/Docker_Burger", (req, res) => {
+    res.render("Docker_Burger")
 })
 
 app.get("/GitHub_Burger", (req, res) => {
