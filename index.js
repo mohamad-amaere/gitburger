92ae92403700bd883ed2e74d89fd9f51abbba8ac
Maram277 <mramalshbli@gmail.com>
2024-10-03 18:27:59 +0200

    create search_query

--- index.js
+++ index.js
@@ -33,6 +33,23 @@ app.get("/Python_Burger", (req, res) => {
     res.render("Python_Burger")
 })
 
+app.get("/", async (req, res) => {
+    const search_query = req.query.serach_burger;
+    let menu = [];
+    if (search_query) {
+        menu = await menuService.searchBurger(search_query);
+    } else {
+        menu = await menuService.getMenu();
+    }
+    res.render("index", { menu});
+});
+
+app.getMaxListeners("/burger/:burgerId", async (req, res) => {
+    const burgerId = req.params.burgerId;
+    const result = await menuService.gitBurgerById(burgerId);
+    res.render("burgerInfo", { burger: result[0]});
+});
+
 
 app.listen(1337)
 
