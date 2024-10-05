8442991ed1c6622f88a9cad1d81ba486addf81c6
mohamad-amaere <maamaere@gmail.com>
2024-10-05 12:04:21 +0200

    Added cart support

--- order_view/index.js
+++ order_view/index.js
@@ -1,37 +1,38 @@
 const express = require("express")
-const menuService  = require("./services/menuService")
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const menuService = require("./services/menuService");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
-app.get("/GitBurger", async (req, res) => {
-    const menu = await menuService.getMenu();
-    res.render("GitBurger", {menu})
-})
-
-app.get("/Menu", (req, res) => {
-    res.render("Menu")
-})
-
 app.get("/", async (req, res) => {
-    const search_query = req.query.serach_burger;
-    let menu = [];
-    if (search_query) {
-        menu = await menuService.searchBurger(search_query);
-    } else {
-        menu = await menuService.getMenu();
-    }
-    res.render("index", { menu});
+	const search_query = req.query.serach_burger;
+	let menu = [];
+	const cart = req.cookies.cart || [];
+
+	if (search_query) {
+		menu = await menuService.searchBurger(search_query);
+	} else {
+		menu = await menuService.getMenu();
+	}
+	res.render("index", { menu, cart });
 });
 
 app.get("/burger/:burgerId", async (req, res) => {
 	const burgerId = req.params.burgerId;
 	const result = await menuService.getBurgerById(burgerId);
 	const ingredients = await menuService.getBurgerIngredients(burgerId);
+	const cart = req.cookies.cart || [];
 
 	res.render("burgerInfo", {
 		burger: result[0],
-		ingredients
+		ingredients,
+		cart
 	});
 });
 
